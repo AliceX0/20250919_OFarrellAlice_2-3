40bfd5e3ccebf1668c81f513359a868721f3eae0
Alice O'Farrell <AliceOFarrell@Outlook.com>
2025-09-18 23:54:58 -0400

    goodbye, whole numbers

---
+++ furniture.py
@@ -0,0 +1,16 @@
+# Furniture.py - This program calculates profits and sales prices for a furniture company.
+
+itemName = "TV Stand"
+retailPrice = 325.00
+wholesalePrice = 200.00
+
+profit = int(retailPrice - wholesalePrice)
+salePrice = float(retailPrice * 0.75)
+saleProfit = float(salePrice - wholesalePrice)
+
+print("Item Name: " + itemName)
+print("Retail Price: $" + str(retailPrice))
+print("Wholesale Price: $" + str(wholesalePrice))
+print("Profit: $" + str(profit))
+print("Sale Price: $" + str(salePrice))
+print("Sale Profit: $" + str(saleProfit))
